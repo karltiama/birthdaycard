fa6cba8d89a99f8c2e45973779e2ece3ac2b6853
Karl Tiama <karltiama@gmail.com>
2024-10-15 23:33:05 -0400

    fix: separate photo data

---
+++ app/data/photoData.ts
@@ -0,0 +1,69 @@
+export interface PhotoItem {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+}
+
+export const photos: PhotoItem[] = [
+  {
+    src: "/images/IMG_7415.jpg",
+    alt: "Sample phone image 1",
+    title: "Pre-Running Selfie",
+    description: "Hardcore Runners about to hit the pavement! I remember you were so excited to run with me, that you said, we are going so slow! Then proceeded to run so fast ahead of me, I am so proud of you running with me!"
+  },
+  {
+    src: "/images/IMG_2763.jpg",
+    alt: "Sample phone image 2",
+    title: "Magnus Omandam?!",
+    description: "I love how silly this picture is, I was laughing so damn hard. To this day I am convinced that you were on the show!"
+  },
+  {
+    src: "/images/IMG_2644.jpg",
+    alt: "Sample phone image 3",
+    title: "What does home mean to you?",
+    description: "Home is not a place, it is a feeling that you experience with the people you love. Thank you for making me feel at home whenever I am with you. Thank you for making me feel welcome and loved."
+  },
+  {
+    src: "/images/IMG_3025.jpg",
+    alt: "Sample phone image 3",
+    title: "Camping Shenanigans",
+    description: "Camping with you was a one in a lifetime experience. From amazing friends, to the freezing nights sharing one little blanket and our bodies to keep warm. I can't wait for the next time we can all be together again."
+  },
+  {
+    src: "/images/IMG_3147.jpg",
+    alt: "Sample phone image 3",
+    title: "You Drank my Margarita?!?",
+    description: "I was being silly and drinking too much, even when you told me to slow down, I am sorry for not listening to you. I really like that taco fest though, it was very fun watching the wrestlers go at it. I could not stop laughing at the fact you kept asking me if this was real? HAHAHA"
+  },
+  {
+    src: "/images/IMG_3164.jpg",
+    alt: "Sample phone image 3",
+    title: "ME + YOU = BOTH BLIND = OUR KIDS SUPER BLIND",
+    description: "This picture brings back bouncy memories. I loved jumping all over the place and absolutely had so much fun jousting each other. Even though you beat my butt multiple times, I still had an amazing time"
+  },
+  {
+    src: "/images/IMG_3169.jpg",
+    alt: "Sample phone image 3",
+    title: "London Sketch Artist",
+    description: "Remind me to never draw you again, at least till I get better. I had so much fun in London, even though London was a small city, we always make the best of it and go on so many adventures."
+  },
+  {
+    src: "/images/IMG_3182.jpg",
+    alt: "Sample phone image 3",
+    title: "Pure Bliss",
+    description: "The funny thing is that before this picture was taken, we had a big fight. I am so glad we were able to make up and make the last of our time in London together worth it. I can't wait for our next chapter together."
+  },
+  {
+    src: "/images/IMG_8796.jpg",
+    alt: "Sample phone image 3",
+    title: "The many faces of Jomai",
+    description: "Honestly the picture that I love the most. Every time I look at the top picture, I laugh my butt off. I love how similar you look with the dogs!"
+  },
+  {
+    src: "/images/museum.jpg",
+    alt: "Sample phone image 3",
+    title: "What a work of art",
+    description: "This picture makes me smile from ear to ear, while some may be looking at the skyline, the main focal point is that cute face of yours hahaha. I love you so much Jomai"
+  }
+];
